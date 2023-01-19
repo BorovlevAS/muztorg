19639532135baa8ae3b27c4a3ff7d18b81aa95eb
Borovlev A. S <bas@qdes.com.ua>
2023-01-19 21:45:23 +0200

    добавил нужные модули

---
+++ kw_invoice_rahf/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Rahunok Faktura Invoice for Ukraine',
+    'summary': 'Rahunok Faktura Рахунок-фактура Рахунок фактура',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Accounting',
+    'license': 'OPL-1',
+    'version': '14.0.1.0.1',
+
+    'depends': ['account', 'kw_invoice_doc_base'],
+
+    'data': [
+        'report/report.xml',
+        'report/templates.xml',
+    ],
+
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+
+}
