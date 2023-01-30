b24d8385a778b5c9efb93e29525a61d772261c7d
Borovlev A. S <bas@qdes.com.ua>
2023-01-30 16:27:06 +0200

    добавил вывод бренда в меню

---
+++ biko_add_brand_to_menu/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "BIKO: Добавляет бренд в меню",
+    "version": "15.0.1.1.0",
+    "author": "Borovlev A.S.",
+    "company": "BIKO Solutions",
+    "depends": ["biko_base_module", "product_brand"],
+    "data": [
+        "views/product_brand_menu.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
