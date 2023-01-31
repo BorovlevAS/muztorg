74047c9d3e41ea7f8421a31d21b666bca68d35ad
Borovlev A. S <bas@qdes.com.ua>
2023-01-31 02:52:31 +0200

    переделал формирование наименования товара

--- biko_auto_create_product_name/__manifest__.py
+++ biko_auto_create_product_name/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "BIKO: Автоматически заполняет название товара в зависимости от полей",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "author": "Borovlev A.S.",
     "company": "BIKO Solutions",
     "depends": ["product"],
--- biko_auto_create_product_name/models/product_template.py
+++ biko_auto_create_product_name/models/product_template.py
@@ -4,36 +4,77 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    def _generate_product_name(self):
-        self.ensure_one()
+    def _generate_product_name(self, vals):
+        
+        if vals['type'] == 'product':
+            brand_name = vals['product_brand_id']
+            model_name = vals['biko_product_model']
+            prefix_name = vals['biko_product_prefix']
 
-        if self.type == 'product':
-            if self.product_brand_id.name:
-                brand_name = self.product_brand_id.name.strip().capitalize()
-            else:
-                brand_name = ''
-
-            if self.biko_product_model:
-                model_name = self.biko_product_model.strip().capitalize()
-            else:
-                model_name = ''
-
-            if self.biko_product_prefix.name:
-                prefix_name = self.biko_product_prefix.name.strip().capitalize()
-            else:
-                prefix_name = ''
-
-            return f'{brand_name} {model_name} {prefix_name}'
+            res = f'{brand_name} {model_name} {prefix_name}'
+            
+            return res
         else:
-            return self.name
+            return vals['name']
 
     @api.onchange('product_brand_id', 'biko_product_prefix', 'biko_product_model')
     def _onchange_product_fields(self):
         for rec in self:
-            rec.name = self._generate_product_name()
+            vals = {
+                'type': rec.type,
+                'name': rec.name,
+                'product_brand_id': rec.product_brand_id.name.strip().upper() if rec.product_brand_id else '',
+                'biko_product_model': rec.biko_product_model.strip().upper() if rec.biko_product_model else '',
+                'biko_product_prefix': rec.biko_product_prefix.name.strip().upper() if rec.biko_product_prefix else '',
+            }
+            rec.name = self._generate_product_name(vals)
+
+    def _gen_vals(self, iscreation, vals):
+
+        product_brand_id = self.env['product.brand'].browse([vals.get('product_brand_id', 
+            self.product_brand_id.id if not iscreation else '')])
+
+        if product_brand_id and product_brand_id.name:
+            product_brand_name = product_brand_id.name.strip().upper()
+        else:
+            product_brand_name = ''
+
+        biko_product_model = vals.get('biko_product_model', 
+            self.biko_product_model if not iscreation else '')
+
+        biko_product_model = biko_product_model.strip().upper() if biko_product_model else ''
+
+        biko_product_prefix_id = vals.get('biko_product_prefix', self.biko_product_prefix.id if not iscreation else '')
+        biko_product_prefix = self.env['biko.product.prefix'].browse([biko_product_prefix_id])
+
+        if biko_product_prefix and biko_product_prefix.name:
+            biko_product_prefix_name = biko_product_prefix.name.strip().upper()
+        else:
+            biko_product_prefix_name = ''
+
+        gen_vals = {
+            'type': vals.get('type', self.type),
+            'name': vals.get('name', self.name),
+            'product_brand_id': product_brand_name,
+            'biko_product_model': biko_product_model,
+            'biko_product_prefix': biko_product_prefix_name,
+        }
+
+        return gen_vals
+
+    @api.model
+    def create(self, vals):
+        
+        gen_vals = self._gen_vals(iscreation=True, vals=vals)
+
+        vals['name'] = self._generate_product_name(gen_vals)
+ 
+        return super(ProductTemplate, self).create(vals)
 
     def write(self, vals):
         
-        vals['name'] = self._generate_product_name()
+        gen_vals = self._gen_vals(iscreation=False, vals=vals)
+
+        vals['name'] = self._generate_product_name(gen_vals)
 
         return super(ProductTemplate, self).write(vals)
