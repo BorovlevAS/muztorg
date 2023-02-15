eca7e30fef73e9928b02934cceab093c099e42dd
Borovlev A. S <bas@qdes.com.ua>
2023-02-16 00:13:11 +0200

    проверка мобильного оператора

--- biko_client_view_ux/models/res_partner.py
+++ biko_client_view_ux/models/res_partner.py
@@ -1,5 +1,53 @@
 from odoo import api, models, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    import phonenumbers
+
+    def check_phone_mobile(number, country_code):
+        try:
+            phone_nbr = phonenumbers.parse(
+                number, region=country_code, keep_raw_input=True
+            )
+
+            op_code = "0" + str(phone_nbr.national_number)[0:2]
+
+            return (
+                op_code
+                in [
+                    "039",
+                    "050",
+                    "063",
+                    "066",
+                    "067",
+                    "068",
+                    "073",
+                    "091",
+                    "092",
+                    "093",
+                    "094",
+                    "095",
+                    "096",
+                    "097",
+                    "098",
+                    "099",
+                ]
+            ), op_code
+
+        except phonenumbers.phonenumberutil.NumberParseException as e:
+            raise UserError(
+                _("Unable to parse %(phone)s: %(error)s", phone=number, error=str(e))
+            )
+
+except ImportError:
+    _logger.error("Error importing module phonenumbers")
+
+    def check_phone_mobile(nomber, country_code):
+        return True
 
 
 class ResPartner(models.Model):
@@ -10,6 +58,15 @@ class ResPartner(models.Model):
 
         for rec in self:
             if rec.mobile and rec.company_type == "person":
+                result, op_code = check_phone_mobile(
+                    rec.mobile, rec.country_id.code if rec.country_id else "UA"
+                )
+
+                if not result:
+                    raise ValidationError(
+                        _("{} is not a mobile operator code").format(op_code)
+                    )
+
                 client_names = (
                     self.env["res.partner"]
                     .search([("mobile", "=", rec.mobile), ("id", "!=", rec.id)])
