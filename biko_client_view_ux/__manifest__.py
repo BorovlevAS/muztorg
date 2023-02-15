eaaa2371478e67329ca9f7029821ccf9b8fd6763
Borovlev A. S <bas@qdes.com.ua>
2023-02-15 18:52:02 +0200

    переделал порядок отображения списка и канбана

---
+++ biko_client_view_ux/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "BIKO: Изменяет UX каталога клиентов",
+    "version": "14.0.1.1.0",
+    "author": "Borovlev A.S.",
+    "company": "BIKO Solutions",
+    "depends": ["contacts"],
+    "data": [
+        "views/contact_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
