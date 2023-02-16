4e5455796bb2e198feab95028d67c6e668ac57ed
Borovlev A. S <bas@qdes.com.ua>
2023-02-16 14:33:56 +0200

    контроль уникальности по коду ЕДРПОУ

---
+++ biko_company_code_unique/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "BIKO: Контроль ЕДРПОУ",
+    "version": "15.0.1.1.0",
+    "author": "Borovlev A.S.",
+    "company": "BIKO Solutions",
+    "depends": ["kw_account_partner_requisites"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
