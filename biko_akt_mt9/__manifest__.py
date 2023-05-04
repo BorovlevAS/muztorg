8c87dec9a05cc128e09f01102efeef7dafaf73b9
bambrik-qd <Bambrik@ukr.net>
2023-05-04 11:18:11 +0300

    Акт приема на экспертизу, ПФ

---
+++ biko_akt_mt9/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Act of acceptance-transfer to service',
+    'summary': 'Act of acceptance-transfer to service Акт прийняття-передачі на сервісне обслуговування',
+
+    'author': 'Zhmyhova',
+    'website': '',
+
+    'category': 'Accounting',
+    'license': 'OPL-1',
+    'version': '14.0.1.0.1',
+
+    'depends': ['crm'], 
+
+    'data': [
+        'report/report.xml',
+        'report/templates.xml',
+    ],
+
+    'installable': True,
+
+    'images': [
+ 
+    ],
+
+}
