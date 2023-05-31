9c42997d00a952ae95e605f87a7345e699f8c547
Borovlev A. S <bas@qdes.com.ua>
2023-05-31 14:30:16 +0300

    быстрый патч для отмены пикингов

---
+++ biko_picking_cancel_patch/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "BIKO: Патч модуля отмены пикингов (делает необязательными поля причины)",
+    "version": "14.0.1.1.0",
+    "author": "Borovlev A.S.",
+    "company": "BIKO Solutions",
+    "depends": [
+        "picking_cancel",
+    ],
+    "data": [],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
