3295b442ec63b6bbf3b904762debe0ed7dbf1d91
Borovlev A. S <bas@qdes.com.ua>
2023-06-02 00:25:27 +0300

    переделал поля в SO

--- biko_client_add_mandatory_contact/models/sale_order.py
+++ biko_client_add_mandatory_contact/models/sale_order.py
@@ -14,7 +14,12 @@ class SaleOrder(models.Model):
     biko_contact_person_id = fields.Many2one(
         "res.partner",
         string="Contact person",
-        domain="[('id', 'in', filter_biko_contact_person_ids)]",
+    )
+
+    biko_contact_person_type = fields.Selection(
+        [("dealer", "Linked to the partner"), ("person", "Any contact from list")],
+        string="Contact filter",
+        default="person",
     )
 
     filter_biko_recipient_ids = fields.Many2many(
@@ -23,10 +28,26 @@ class SaleOrder(models.Model):
     biko_recipient_id = fields.Many2one(
         "res.partner",
         string="Recipient person",
-        domain="[('id', 'in', filter_biko_recipient_ids)]",
+    )
+
+    biko_recipient_type = fields.Selection(
+        [("dealer", "Linked to the partner"), ("person", "Any contact from list")],
+        string="Recipient filter",
+        default="person",
     )
 
     partner_id_domain = fields.Char(compute="_compute_partner_id_domain", readonly=True, store=False)
+    biko_recepient_domain = fields.Char(
+        compute="_compute_biko_recepient_domain",
+        readonly=True,
+        store=False,
+    )
+
+    biko_contact_domain = fields.Char(
+        compute="_compute_biko_contact_domain",
+        readonly=True,
+        store=False,
+    )
 
     @api.depends("biko_dealer_id")
     def _compute_partner_id_domain(self):
@@ -50,16 +71,26 @@ class SaleOrder(models.Model):
                     ]
                 )
 
+    @api.depends("biko_recipient_type")
+    def _compute_biko_recepient_domain(self):
+        for rec in self:
+            if rec.biko_recipient_type == "dealer":
+                rec.biko_recepient_domain = json.dumps([("id", "in", rec.filter_biko_recipient_ids.ids)])
+            else:
+                rec.biko_recepient_domain = json.dumps([("id", "!=", False)])
+
+    @api.depends("biko_contact_person_type")
+    def _compute_biko_contact_domain(self):
+        for rec in self:
+            if rec.biko_contact_person_type == "dealer":
+                rec.biko_contact_domain = json.dumps([("id", "in", rec.filter_biko_contact_person_ids.ids)])
+            else:
+                rec.biko_contact_domain = json.dumps([("id", "!=", False)])
+
     @api.onchange("partner_id")
     def _onchange_partner_id(self):
         for rec in self:
             if rec.partner_id.biko_carrier_id:
                 rec.carrier_id = rec.partner_id.biko_carrier_id
-
-    """
-    дилер - может быть пустым - это всегда с типом компания
-    покупатель от дилера или физлицо
-    контактное лицо - от дилера или от физлица
-    получатель - от дилера или от физлица
-    адрес доставки - от дилера или от физлица
-    """
+            rec.biko_recipient_type = "dealer" if rec.partner_id.is_company else "person"
+            rec.biko_contact_person_type = "dealer" if rec.partner_id.is_company else "person"
