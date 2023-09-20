c0bd332277043cee7808a055e0f97df7dbe0a0ee
bambrik-qd <Bambrik@ukr.net>
2023-09-20 10:17:13 +0300

    [ADD](T2566) muztorg_so_set_route: Встановлення значення маршрут при створенні sale.order

---
+++ muztorg_so_set_route/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "BIKO: Встановлення значення маршрут при створенні sale.order",
+    "version": "14.0.1.0.0",
+    "author": "Zhmyhova T.N.",
+    "company": "BIKO Solutions",
+    "depends": [
+        "stock",
+        "sale",
+    ],
+    "data": [
+        "views/stock_views.xml",
+        ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
