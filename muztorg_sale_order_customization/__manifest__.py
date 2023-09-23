b0f307e6f6f6b1111a4dca9c88741c3c8dbeb54f
Borovlev A. S <bas@qdes.com.ua>
2023-09-23 14:40:29 +0300

    [ADD] (T2601) muztorg_sale_order_customization: new module for sale_delay added

---
+++ muztorg_sale_order_customization/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "BIKO: Sale Order Customization (MUZTORG)",
+    "version": "14.0.1.0.0",
+    "author": "Borovlev A.S.",
+    "company": "BIKO Solutions",
+    "depends": [
+        "sale",
+        "sale_stock",
+    ],
+    "data": [],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
