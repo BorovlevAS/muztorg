947ad2eec295db2522415b9cbb5b140eea8eab59
Borovlev A. S <bas@qdes.com.ua>
2023-10-18 18:00:04 +0300

    [IMP] (T2942) biko_np_patch: backword payment disabled

--- biko_np_patch/__manifest__.py
+++ biko_np_patch/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "BIKO: Патч модуля Новой Почты под требования МУЗТОРГ",
-    "version": "14.0.1.1.21",
+    "version": "14.0.1.1.22",
     "author": "Borovlev A.S.",
     "company": "BIKO Solutions",
     "depends": [
