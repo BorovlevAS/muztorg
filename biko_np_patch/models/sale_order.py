34298f1c8df6be729bf5568e871a694bb3c4ccd9
@ztn <ztn@qdes.com.ua>
2023-11-08 23:17:26 +0200

    [IMX] (T3114) biko_np_patch: поля Контроль оплаты и сумма для контроля оплаты в документе  stock.picking доступны для редактирования

--- biko_np_patch/models/sale_order.py
+++ biko_np_patch/models/sale_order.py
@@ -1,17 +1,24 @@
-from odoo import fields, models
-
-# from odoo.exceptions import ValidationError
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    afterpayment_check = fields.Boolean(string="Afterpayment check", default=False)
+    afterpayment_check = fields.Boolean(
+        string="Afterpayment check",
+        inverse="_inverse_afterpayment_check",
+        store=True,
+        default=False,
+    )
     backward_money_costs = fields.Float(
         "Amount to control payment (UAH)",
         compute="_compute_backward_money_costs",
+        inverse="_inverse_backward_money_costs",
+        store=True,
+        readonly=False,
     )
 
+    @api.depends("amount_total")
     def _compute_backward_money_costs(self):
         cost = self.amount_total
         date = self.date_order or fields.Date.today()
@@ -23,3 +30,15 @@ class SaleOrder(models.Model):
         if currency_uah != order_currency:
             cost = currency_uah.compute(cost, order_currency)
         self.backward_money_costs = cost
+
+    def _inverse_afterpayment_check(self):
+        for order in self:
+            for stock in order.picking_ids:
+                if stock.state != "done":
+                    stock.update({"afterpayment_check": order.afterpayment_check})
+
+    def _inverse_backward_money_costs(self):
+        for order in self:
+            for stock in order.picking_ids:
+                if stock.state != "done":
+                    stock.update({"backward_money_costs": order.backward_money_costs})
