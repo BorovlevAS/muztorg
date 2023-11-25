03073be08c1e36a1dae8b78c4f8021cd14acdd4f
Borovlev A. S <bas@qdes.com.ua>
2023-11-26 01:28:53 +0200

    Add module for changing product tax settings

---
+++ muztorg_change_product_tax_settings/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "BIKO: Change product taxes",
+    "version": "14.0.1.0.0",
+    "author": "Borovlev A.S.",
+    "company": "BIKO Solutions",
+    "depends": [],
+    "data": [],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
