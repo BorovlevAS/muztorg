873e17120e59f53c864410d0b291aff2ccca3b73
@ztn <ztn@qdes.com.ua>
2023-11-30 20:32:02 +0200

    [IMX] (T3396) muztorg_sale_order_customization: добавлены поля biko_website_ref и biko_1c_ref

--- muztorg_sale_order_customization/__manifest__.py
+++ muztorg_sale_order_customization/__manifest__.py
@@ -1,13 +1,15 @@
 {
     "name": "BIKO: Sale Order Customization (MUZTORG)",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "author": "Borovlev A.S.",
     "company": "BIKO Solutions",
     "depends": [
         "sale",
         "sale_stock",
     ],
-    "data": [],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
     "license": "LGPL-3",
     "installable": True,
     "application": True,
