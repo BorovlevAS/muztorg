60806f5958fd73c08ed24f4f8b809fcca08c5f56
Borovlev A. S <bas@qdes.com.ua>
2023-12-05 01:48:29 +0200

    [FIX] biko_auto_create_product_name: dependencies fixed

--- biko_auto_create_product_name/__manifest__.py
+++ biko_auto_create_product_name/__manifest__.py
@@ -1,10 +1,14 @@
-# -*- coding: utf-8 -*-
 {
     "name": "BIKO: Автоматически заполняет название товара в зависимости от полей",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.1.2",
     "author": "Borovlev A.S.",
     "company": "BIKO Solutions",
-    "depends": ["product"],
+    "depends": [
+        "product",
+        "product_brand",
+        "biko_add_product_class",
+        "biko_add_product_prefix",
+    ],
     "data": [],
     "license": "LGPL-3",
     "installable": True,
