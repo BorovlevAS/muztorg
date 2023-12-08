39d7bfde1710b43aa0f887cff07ad8134d084893
Borovlev A. S <bas@qdes.com.ua>
2023-12-08 22:28:51 +0200

    [IMP] (T0486) muztorg_sale_order_customization: payment type added to the sale.order

--- muztorg_sale_order_customization/__manifest__.py
+++ muztorg_sale_order_customization/__manifest__.py
@@ -1,14 +1,19 @@
 {
     "name": "BIKO: Sale Order Customization (MUZTORG)",
-    "version": "14.0.2.0.0",
+    "version": "14.0.3.0.0",
     "author": "Borovlev A.S.",
     "company": "BIKO Solutions",
     "depends": [
         "sale",
         "sale_stock",
+        "biko_base_module",
+        "account",
     ],
     "data": [
         "views/sale_order_views.xml",
+        "views/so_payment_type_views.xml",
+        "security/ir.model.access.csv",
+        "data/so_payment_types.xml",
     ],
     "license": "LGPL-3",
     "installable": True,
---
+++ muztorg_sale_order_customization/models/so_payment_type.py
@@ -0,0 +1,16 @@
+from odoo import fields, models
+
+
+class SOPaymentType(models.Model):
+    _name = "so.payment.type"
+    _description = "Sale Order Payment Type"
+
+    _rec_name = "name"
+    _order = "sequence"
+
+    active = fields.Boolean(string="Active", default=True)
+
+    name = fields.Char(string="Name", required=True, copy=False)
+
+    is_prepayment = fields.Boolean(string="Is Prepayment")
+    sequence = fields.Integer(string="Sequence")
