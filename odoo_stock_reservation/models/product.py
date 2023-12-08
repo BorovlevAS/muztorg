277723e796ac1fc8380290cb6cf3657281b9ae65
Borovlev A. S <bas@qdes.com.ua>
2023-12-09 01:37:07 +0200

    [DEL] (T0486) removed unnecessary modules

--- odoo_stock_reservation/models/product.py
+++
@@ -1,9 +0,0 @@
-from odoo import _, api, models, fields
-
-
-class Product(models.Model):
-    _inherit = 'product.template'
-
-    is_reservation = fields.Boolean(default=False, string='Is Reserved')
-
-    reserver_id = fields.Many2one('res.users')
