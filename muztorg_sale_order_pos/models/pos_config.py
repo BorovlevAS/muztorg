89de7de67d4d8911e18f40214e484446e7a42992
Borovlev A. S <bas@qdes.com.ua>
2023-12-14 22:22:32 +0200

    [ADD] (T3539) muztorg_sale_order_pos: draft module

---
+++ muztorg_sale_order_pos/models/pos_config.py
@@ -0,0 +1,45 @@
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    _inherit = "pos.config"
+
+    is_auto_open_pos = fields.Boolean(
+        string="Auto Open POS interface",
+        default=False,
+    )
+
+    is_use_with_sale_order = fields.Boolean(
+        string="Use with Sale Order",
+        default=False,
+    )
+
+    department_ids = fields.Many2many(
+        comodel_name="hr.department",
+        string="Departments",
+    )
+
+    def open_session_cb(self, check_coa=True):
+        """new session button
+
+        create one if none exist
+        access cash control interface if enabled or start a session
+        """
+        self.ensure_one()
+        if not self.current_session_id:
+            self._check_pricelists()
+            self._check_company_journal()
+            self._check_company_invoice_journal()
+            self._check_company_payment()
+            self._check_currencies()
+            self._check_profit_loss_cash_journal()
+            self._check_payment_method_ids()
+            self._check_payment_method_receivable_accounts()
+            self.env["pos.session"].create(
+                {"user_id": self.env.uid, "config_id": self.id}
+            )
+
+        if self.is_auto_open_pos:
+            return self.open_ui()
+
+        return True
