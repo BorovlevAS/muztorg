d9eae93ffb442a45026b16bdca823942d52a648c
Borovlev A. S <bas@qdes.com.ua>
2024-01-02 16:19:16 +0200

    [ADD] (T3777) muztorg_service_addon: module added

---
+++ muztorg_service_addon/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "BIKO: Service Addon with different server actions",
+    "version": "14.0.1.0.0",
+    "author": "Borovlev A.S.",
+    "company": "BIKO Solutions",
+    "depends": [
+        "account",
+        "sale",
+        "sale_automatic_workflow",
+    ],
+    "data": [
+        "views/account_move_actions.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
