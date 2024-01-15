69ab98118de0ea2a6340740087e6f3417147f807
Borovlev A. S <bas@qdes.com.ua>
2024-01-15 11:45:07 +0200

    [ADD] (T3694) purchase subscriptions: modules added

---
+++ muztorg_purchase_subscription/models/__init__.py
@@ -0,0 +1,13 @@
+from . import (
+    account_move,
+    product_template,
+    purchase_order,
+    purchase_order_line,
+    purchase_subscription,
+    purchase_subscription_close_reason,
+    purchase_subscription_line,
+    purchase_subscription_template,
+    res_partner,
+    sale_subscription_stage,
+    sale_subscription_tag,
+)
