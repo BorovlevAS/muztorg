e0aad03e658002c8f8dff1efc853334d56d4d4cf
Borovlev A. S <bas@qdes.com.ua>
2024-01-20 00:32:21 +0200

    [IMP] (T3973) muztorg_sale_order_customization: margin rules added

--- muztorg_sale_order_customization/__manifest__.py
+++ muztorg_sale_order_customization/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "BIKO: Sale Order Customization (MUZTORG)",
-    "version": "14.0.4.2.0",
+    "version": "14.0.5.2.0",
     "author": "Borovlev A.S.",
     "company": "BIKO Solutions",
     "depends": [
@@ -9,11 +9,13 @@
         "biko_base_module",
         "account",
         "product_stock_by_location_widget",
+        "sale_margin",
     ],
     "data": [
         "views/sale_order_views.xml",
         "views/so_payment_type_views.xml",
         "views/res_config_settings_views.xml",
+        "security/ir_access_roles.xml",
         "security/ir.model.access.csv",
         "data/so_payment_types.xml",
     ],
