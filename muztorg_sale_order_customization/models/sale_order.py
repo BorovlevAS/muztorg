f38af6e0a8e9bd60b1a6bc48e8659fd29805ca1c
Borovlev A. S <bas@qdes.com.ua>
2024-01-26 18:37:55 +0200

    [IMP] (T3792_1) muztorg_sale_order_customization: write method refactored

--- muztorg_sale_order_customization/models/sale_order.py
+++ muztorg_sale_order_customization/models/sale_order.py
@@ -35,24 +35,7 @@ class SaleOrder(models.Model):
         if from_curr == to_curr:
             return
 
-        for order_line in record.order_line:
-            price_unit = from_curr._convert(
-                order_line["price_unit"],
-                to_curr,
-                company_id,
-                record.date_order,
-            )
-            _logger.info(
-                "MUZTORG_SALE_ORDER_CUSTOMIZATION: {old_price} -> {new_price}".format(
-                    old_price=order_line["price_unit"],
-                    new_price=price_unit,
-                )
-            )
-            order_line.write({"price_unit": price_unit})
-
-        record.with_context(skip_recalculation_currency=True).write(
-            {"pricelist_id": pricelist_uah.id}
-        )
+        record.write({"pricelist_id": pricelist_uah.id})
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -62,12 +45,3 @@ class SaleOrder(models.Model):
             record.recalculate_prices()
 
         return result
-
-    # def write(self, vals):
-    #     result = super().write(vals)
-
-    #     if not self.env.context.get("skip_recalculation_currency", False):
-    #         for record in self:
-    #             record.recalculate_prices()
-
-    #     return result
