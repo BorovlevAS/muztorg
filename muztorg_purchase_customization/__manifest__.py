31ba986004d2e908464f4c629d11f985bb78a559
anastasiia <evdokimova.arh@gmail.com>
2024-03-03 14:32:36 +0200

    [IMP] (T4362) muztorg_purchase_customization:fields uktzed, biko_country_name are displayed on the purchase form

--- muztorg_purchase_customization/__manifest__.py
+++ muztorg_purchase_customization/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "Muztorg: Правки 'purchase.order' под требования МУЗТОРГ",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "author": "Zhmyhova T.N.",
     "company": "BIKO Solutions",
     "depends": [
         "purchase_stock",
+        "biko_add_product_class",
     ],
     "data": [
         "views/purchase_views.xml",
